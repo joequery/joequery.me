81a072b2db896879e09fc8aacaf1d0892000cd75
Joseph McCullough <joseph@vertstudios.com>
2012-12-06 03:53:50 -0600

    Add resume content and remove github static page

--- joequery/settings.py
+++ joequery/settings.py
@@ -1,12 +1,14 @@
 # Global setting variables and etc go here.
 import os
 from flask import Flask, request, g, render_template
+from flaskext.markdown import Markdown
 
 SECRET_KEY = "9x1P17j12gbQsCL4xqyQDp1kkOR7l5vMgxsw6elhkd49jxMIvwm8QBlNiSv2tBy-pIpwZf5RBZFB65qJV5ZNTetEhboDJ5GnWVSdZ_dnLtA="
 
 # App configuration
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
+Markdown(app)
 
 # FLASK_ENV environment variable can be 'development' or 'production'
 if "FLASK_ENV" in os.environ.keys():
