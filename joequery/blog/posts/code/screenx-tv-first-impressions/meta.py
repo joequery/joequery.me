5894eca871f4cf86bc4bf05b3bf852025c05bfb7
Joseph McCullough <joseph@vertstudios.com>
2012-12-08 03:02:46 -0600

    Complete screenxtv first impressions article

---
+++ joequery/blog/posts/code/screenx-tv-first-impressions/meta.py
@@ -0,0 +1,7 @@
+title="ScreenX TV: First Impressions"
+description="""
+My initial thoughts of [ScreenX TV](http://screenx.tv), a way to broadcast
+your terminal to the world.
+"""
+time="2012-12-07 Fri 09:53 PM"
+# related=[("Some article", "its/url")]
