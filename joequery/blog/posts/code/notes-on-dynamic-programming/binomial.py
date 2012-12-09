f74a7741642ea1135da1e664a26fb9906ea96c08
Joseph McCullough <joseph@vertstudios.com>
2012-12-08 18:23:29 -0600

    Begin working on dynamic programming post

---
+++ joequery/blog/posts/code/notes-on-dynamic-programming/binomial.py
@@ -0,0 +1,19 @@
+def binomial(n,k):
+    C = []
+    i = 0
+    x = 0
+    while i <= n:
+        j = 0
+        C.insert(i, [])
+        while j <= min(i,k):
+            if j == 0 or j == i:
+                C[i].insert(j, 1)
+            else:
+                C[i].insert(j, C[i-1][j-1] + C[i-1][j])
+            j += 1
+            x += 1
+        i += 1
+    print("Additions: %d" % x)
+    return C[n][k]
+
+print(binomial(5,2)) # 10 
---
+++ joequery/blog/posts/code/notes-on-dynamic-programming/meta.py
@@ -0,0 +1,6 @@
+title="Notes on dynamic programming"
+description="""
+Textbook notes on dynamic programming theory and applications.
+"""
+time="2012-12-08 Sat 06:20 PM"
+# related=[("Some article", "its/url")]
