29e841cc8f2ec5a38f3ac881863b61e055c74507
Joseph McCullough <joseph@vertstudios.com>
2012-12-10 02:44:40 -0600

    Change dynamic programming url to have part-1

---
+++ joequery/blog/posts/code/notes-on-dynamic-programming-part-1/meta.py
@@ -0,0 +1,9 @@
+title="Notes on dynamic programming - part 1"
+description=""" 
+Part 1 of extensive notes discussing the fundamentals of dynamic programming.
+Examples in these notes include the Fibonacci sequence and Warshall's
+algorithm. Pseudocode and Python implementations of the algorithms are
+provided.
+"""
+time="2012-12-10 Mon 02:28 AM"
+# related=[("Some article", "its/url")]
--- joequery/blog/posts/code/notes-on-dynamic-programming/meta.py
+++
@@ -1,6 +0,0 @@
-title="Notes on dynamic programming"
-description="""
-Textbook notes on dynamic programming theory and applications.
-"""
-time="2012-12-10 Mon 02:28 AM"
-# related=[("Some article", "its/url")]
