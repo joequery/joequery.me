5ca96beb26dd2ab5285a57f5cade6f01160df368
Joseph McCullough <joseph@vertstudios.com>
2012-12-10 02:48:33 -0600

    Update description and timestamp for dynamic programming part 1

--- joequery/blog/posts/code/notes-on-dynamic-programming-part-1/meta.py
+++ joequery/blog/posts/code/notes-on-dynamic-programming-part-1/meta.py
@@ -1,9 +1,9 @@
 title="Notes on dynamic programming - part 1"
 description=""" 
 Part 1 of extensive notes discussing the fundamentals of dynamic programming.
-Examples in these notes include the Fibonacci sequence and Warshall's
-algorithm. Pseudocode and Python implementations of the algorithms are
+Examples in these notes include the Fibonacci sequence, the Binomial Formula,
+and Warshall's algorithm. Python implementations of the algorithms are
 provided.
 """
-time="2012-12-10 Mon 02:28 AM"
+time="2012-12-10 Mon 02:48 AM"
 # related=[("Some article", "its/url")]
