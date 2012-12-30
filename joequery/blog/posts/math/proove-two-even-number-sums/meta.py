db311f82362684a155a79baca7bf86702a724a54
Joseph McCullough <joseph@vertstudios.com>
2012-12-30 05:32:13 -0600

    Adding two even numbers is even article

---
+++ joequery/blog/posts/math/proove-two-even-number-sums/meta.py
@@ -0,0 +1,6 @@
+title="Proving the sum of two even numbers is even"
+description="""
+A simple proof of the equally simple claim that the sum of two even numbers is
+also even.  """
+time="2012-12-30 Sun 05:09 AM"
+# related=[("Some article", "its/url")]
