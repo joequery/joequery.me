eda1da5aa8434caa233b1697180ea815c6042e5e
Joseph McCullough <joseph@vertstudios.com>
2013-01-03 03:31:50 -0600

    Changed recent articles from meta.py to meta.txt

--- joequery/blog/posts/code/resolve-uwsgi-symbolic-link-issue/meta.py
+++
@@ -1,5 +0,0 @@
-title="Resolving UWSGI symbolic link issues"
-description="""How to solve UWSGI runtime issues that occur when using symbolic
-links for the project directory."""
-time="2012-12-31 Mon 06:03 PM"
-# related=[("Some article", "its/url")]
