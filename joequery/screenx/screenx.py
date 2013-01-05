3c5904d7bb5069cf5e756e92c2aca832c16a81e6
Joseph McCullough <joseph@vertstudios.com>
2013-01-05 01:25:58 -0600

    Make sure cache is initialized

--- joequery/screenx/screenx.py
+++ joequery/screenx/screenx.py
@@ -33,11 +33,17 @@ def screenx_cache_get(k):
     if UWSGI_ENV:
         v = int(uwsgi.cache_get(k))
     else:
-        v = SCREENX_CACHE[k]
+        v = SCREENX_CACHE.get(k)
     return v
 
 def screenx_check_status():
   t = int(time.time())
+  
+  # Make sure the cache has been initialized
+  if screenx_cache_get('streaming') is None:
+      screenx_cache_set('streaming', False)
+      screenx_cache_set('lastChecked', -1)
+
   if screenx_cache_expired(t):
       screenx_cache_set('lastChecked', t)
       r = requests.get("http://screenx.tv/screens/status/joequery")
